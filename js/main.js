babf2729470a19c49866cea416c80e81a6de6a73
Atul Bhardwaj <atul.bhardwaj07@gmail.com>
2021-06-19 21:31:05 +0530

    Validation rules

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
-(function($) {
+(function ($) {
     "use strict";
-    $('.input100').each(function() {
-        $(this).on('blur', function() {
-            if ($(this).val().trim() != "") {
-                $(this).addClass('has-val');
-            } else {
-                $(this).removeClass('has-val');
-            }
-        })
-    })
-    var input = $('.validate-input .input100');
-    $('.validate-form').on('submit', function() {
+
+    // $('.input100').each(function() {
+    //     $(this).on('blur', function() {
+    //         if ($(this).val().trim() != "") {
+    //             $(this).addClass('has-val');
+    //         } else {
+    //             $(this).removeClass('has-val');
+    //         }
+    //     })
+    // })
+
+    var input = $('.custom-validation-input');
+
+    $('.validate-form').on('submit', function () {
         var check = true;
         for (var i = 0; i < input.length; i++) {
             if (validate(input[i]) == false) {
@@ -20,8 +23,9 @@
         }
         return check;
     });
-    $('.validate-form .input100').each(function() {
-        $(this).focus(function() {
+
+    $('.custom-validation-input').each(function () {
+        $(this).focus(function () {
             hideValidate(this);
         });
     });
@@ -31,6 +35,20 @@
             if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
                 return false;
             }
+        } else if ($(input).attr('type') == 'number') {
+            if ($(input).val().trim() == '') {
+                return false;
+            } else if ($(input).hasClass("max-marks-100") && $(input).val() > 100) {
+                return false;
+            } else if ($(input).hasClass("max-marks-1000") && $(input).val() > 1000) {
+                return false;
+            } else if ($(input).hasClass("marks-obtained-100")) {
+                if (Number($(input).parent().find(".max-marks-100").val()) < Number($(input).val()))
+                    return false;
+            } else if ($(input).hasClass("marks-obtained-1000")) {
+                if (Number($(input).parent().find(".max-marks-1000").val()) < Number($(input).val()))
+                    return false;
+            }
         } else {
             if ($(input).val().trim() == '') {
                 return false;
@@ -39,12 +57,20 @@
     }
 
     function showValidate(input) {
-        var thisAlert = $(input).parent();
-        $(thisAlert).addClass('alert-validate');
+        var thisParentAlert = $(input).parent();
+        var thisGrandParentAlert = $(input).parent().parent();
+        if ($(input).parent().hasClass("custom-validation-child"))
+            $(thisParentAlert).addClass('alert-validate');
+        else
+            $(thisGrandParentAlert).addClass('alert-validate');
     }
 
     function hideValidate(input) {
-        var thisAlert = $(input).parent();
-        $(thisAlert).removeClass('alert-validate');
+        var thisParentAlert = $(input).parent();
+        var thisGrandParentAlert = $(input).parent().parent();
+        if ($(input).parent().hasClass("custom-validation-child"))
+            $(thisParentAlert).removeClass('alert-validate');
+        else
+            $(thisGrandParentAlert).removeClass('alert-validate');
     }
 })(jQuery);
